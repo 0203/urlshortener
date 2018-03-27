e2cf9acc7cd114948d517280f4029dff7ac4421b
Glitch (izraurlshortener) <none>
2018-03-27 07:36:37 +0000

    🍹👑 Checkpoint ./package.json:762202/1354 ./server.js:762202/401

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const mongoose = require("mongoose")
 
 const app = express()
 app.use(bodyParser.json())
