cf76b061768a138e787399ed7145a00d4e6be2a2
Glitch (izraurlshortener) <none>
2018-03-27 09:04:40 +0000

    ⭐️🍟 Checkpoint ./server.js:762202/79

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(express.static(__dirname + '/views'))
 app.get("/:urlToShorten(*)", (request, response, next) => {
   var { urlToShorten } = request.params;
   
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b
+  
   response.json({urlToShorten});
 })
  
