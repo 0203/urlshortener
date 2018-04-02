a994818dc1aa6ba9beb893d3d1ed1b913f46983e
Glitch (izraurlshortener) <none>
2018-04-02 01:51:43 +0000

    🎱🚋 Checkpoint ./server.js:762202/4667 ./models/shortUrl.js:762202/513 ./views/index.html:762202/372

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
-  originalUrl: String,
-  shorterUrl: String
-}, {timestamp:true});
+  originalUrl : String,
+  shorterUrl : String
+},{timestamps:true});
 
 const ModelClass = mongoose.model('shortUrl', urlSchema);
 
--- server.js
+++ server.js
@@ -3,42 +3,90 @@
 
 // init project
 const express = require('express')
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const shortUrl = require("./models/shortUrl")
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express();
+const shortUrl = require("./model/shortUrl")
 
-const app = express()
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use(cors()) 
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
 
-//connect to database
-mongoose.connect(process.env.MONGODB_URI);
+//Connet to database
+mongoose.connect("mongodb://ammar:1111@ds121999.mlab.com:21999/apitest").then(
+  () => { console.log("connected") },
+  err => { console.log(err) }
+);
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static(__dirname + 'public'))
+// app.use(express.static(__dirname + 'public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/new/:urlToShorten(*)", (request, response, next) => {
-  var urlToShorten = request.params.urlToShorten;
+// Simple in-memory store
+app.get('/new/:urlToShorten(*)',(req,res,next)=>{
+  var urlToShorten = req.params.urlToShorten;
   
   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
   
-  return response.json({urlToShorten});
-})
- 
+  var regex = expression;
+  
+ if(regex.test(urlToShorten)===true) {
+   var short = Math.floor(Math.random() * 100000).toString(); 
+   
+   var data = new shortUrl({
+     originalUrl : urlToShorten,
+     shorterUrl : short
+   });
+   
+   data.save(function(err){
+   if(err){
+     return res.send("Error save to database");
+   }
+   });
+   
+ return res.json(data)
+ }
+  
+  var data = new shortUrl({
+  originalUrl: 'URL entered does not follow the correct format',
+  shorterUrl : 'Invalid URL'
+  });
+  
+  return res.json(data);
+});
 
+//Query database and forward to originalURL
+app.get ("/:urlToForward",function (req,res,next){
+ var shorterUrl = req.params.urlToForward;
+         
+shortUrl.findOne({'shorterUrl': shorterUrl},function(err,data)
+{
+  if (err) return res.send("Error reading database");
+  
+  var re = new RegExp ("^(http|https)://", "i");
+  var strToCheck = data.originalUrl;
+  
+  if(re.test(strToCheck)){
+    res.redirect(301,data.originalUrl);
+  }
+  else
+  {
+     res.redirect(301, 'http://' + data.originalUrl);
+  }
+
+});
+         });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
 })
+
